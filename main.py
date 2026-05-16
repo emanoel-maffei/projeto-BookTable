073ceab86cfa50e684758f98f69b40f1f17e175c
Emanoel Brian Maffei <emanoel.maffei@escola.pr.gov.br>
2026-05-16 15:06:31 -0300

    Melhora a arquitetura do código

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ ctk.set_appearance_mode("Light")
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
+        
+        ###########################################
+        ## Configurações iniciais da janela raiz ##
+        ###########################################
 
         self.title("BookTable")
         self.geometry("600x500")
@@ -19,41 +23,55 @@ class App(ctk.CTk):
         # self.resizable(True, True) # Já por padrão
         # self.state("zoomed")
 
+        
+        ############
         ## Fontes ##
+        ############
 
-        # Fonte para Label em negrito
+        # Para Label simples
         self.label_font = CTkFont(
             size=16,
             family="Arial"
         )
 
-        # Fonte para Label simples
+        # Para Label em negrito
         self.label_font_bold = CTkFont(
             size=16,
             family="Arial",
             weight="bold"
         )
 
+        #############################################
+        ## Criando a interface por meio de classes ##
+        #############################################
+        
+        self.tela_login = TelaLogin(master=self)
+        self.tela_login.pack(
+            expand=True,
+            fill="both"
+        )
+
+class TelaLogin(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
         # Configurações da Grade (Grid)
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure((1, 2), weight=1)
 
-        # Chamada das funções construtoras da interface 
-        self.criar_tela_login()
-
-    def criar_tela_login(self):
         # Switch -> Alternador do Modo Escuro
-        switch_modo_escuro = ctk.CTkSwitch(
+        self.switch_modo_escuro = ctk.CTkSwitch(
             master=self,
-            text="Modo Escuro"
+            text="Modo Escuro",
+            command=self.alternar_modo_escuro
         )
-        switch_modo_escuro.place(
+        self.switch_modo_escuro.place(
             x=10, 
             y=10,
         )
 
         # Label -> Título Principal
-        ctk.CTkLabel(
+        self.label_titulo = ctk.CTkLabel(
             master=self,
             text="BookTable",
             text_color="white",
@@ -63,7 +81,8 @@ class App(ctk.CTk):
                 size=44,
                 family="Arial"
             )
-        ).grid(
+        )
+        self.label_titulo.grid(
             row=0, 
             column=0,
             ipadx=16, 
@@ -72,78 +91,83 @@ class App(ctk.CTk):
         )
 
         # Frame -> Container Campo Usuário
-        frame_usuario = ctk.CTkFrame(
+        self.frame_usuario = ctk.CTkFrame(
             master=self,
             fg_color="transparent"
         )
-        frame_usuario.grid(row=1, column=0, sticky="nsew")
+        self.frame_usuario.grid(row=1, column=0, sticky="nsew")
 
         # frame_usuario > Frame -> Centralizador do Rótulo e Campo
-        frame_usuario_centralizador = ctk.CTkFrame(
-            master=frame_usuario,
+        self.frame_usuario_centralizador = ctk.CTkFrame(
+            master=self.frame_usuario,
             fg_color="transparent",
         )
-        frame_usuario_centralizador.place(
+        self.frame_usuario_centralizador.place(
             relx=0.5,
             rely=0.5,
             anchor="center"
         )
 
         # frame_usuario > Label -> Rótulo para o Campo do Usuário
-        ctk.CTkLabel(
-            master=frame_usuario_centralizador,
+        self.label_usuario = ctk.CTkLabel(
+            master=self.frame_usuario_centralizador,
             text="Usuário:",
             font=CTkFont(
                 size=16,
                 family="Arial",
                 weight="bold"
             )
-        ).pack(pady=(0, 20))
+        )
+        self.label_usuario.pack(pady=(0, 20))
 
         # frame_usuario > Entry:
-        entry_usuario = ctk.CTkEntry(
-            master=frame_usuario_centralizador,
+        self.entry_usuario = ctk.CTkEntry(
+            master=self.frame_usuario_centralizador,
             placeholder_text="Digite seu email.",
             width=300
         )
-        entry_usuario.pack(padx=(16, 16))
+        self.entry_usuario.pack(padx=(16, 16))
 
         # Frame -> Container Campo Senha
-        frame_senha = ctk.CTkFrame(
+        self.frame_senha = ctk.CTkFrame(
             master=self,
             fg_color="transparent"
         )
-        frame_senha.grid(row=2, column=0, sticky="nsew")
+        self.frame_senha.grid(row=2, column=0, sticky="nsew")
 
         # frame_senha > Frame -> Centralizador do Rótulo e Campo da Senha
-        frame_senha_centralizador = ctk.CTkFrame(
-            master=frame_senha,
+        self.frame_senha_centralizador = ctk.CTkFrame(
+            master=self.frame_senha,
             fg_color="transparent"
         )
-        frame_senha_centralizador.place(
+        self.frame_senha_centralizador.place(
             relx=0.5,
             rely=0.5,
             anchor="center"
         )
 
         # frame_senha > Label -> Rótulo do Campo da Senha
-        ctk.CTkLabel(
-            master=frame_senha_centralizador,
+        self.label_senha = ctk.CTkLabel(
+            master=self.frame_senha_centralizador,
             text="Senha:",
             font=CTkFont(
                 size=16,
                 family="Arial",
                 weight="bold"
             )
-        ).pack(pady=(0, 20))
+        )
+        self.label_senha.pack(pady=(0, 20))
 
         # frame_senha > Entry -> Campo da Senha
-        entry_senha = ctk.CTkEntry(
-            master=frame_senha_centralizador,
+        self.entry_senha = ctk.CTkEntry(
+            master=self.frame_senha_centralizador,
             placeholder_text="Digite a senha.",
             width=300
         )
-        entry_senha.pack(padx=(16, 16))
+        self.entry_senha.pack(padx=(16, 16))
+
+    def alternar_modo_escuro(self):
+        ctk.set_appearance_mode("Dark" if self.switch_modo_escuro.get() else "Light")
 
 root = App()
 root.mainloop()
