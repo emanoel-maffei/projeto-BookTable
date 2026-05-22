931bdd59c2cc89aac65b739118acb55e8718c011
Emanoel Brian Maffei <emanoel.maffei@escola.pr.gov.br>
2026-05-22 07:59:13 -0300

    Altera detalhes adicionais da barra lateral

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from app_gui import App
 # Para limpar o terminal e facilitar a leitura das informações imprimidas ao decorrer do código
 subprocess.run("cls", shell=True)
 
-ctk.set_appearance_mode("Dark")
+ctk.set_appearance_mode("Light")
 ctk.set_default_color_theme("./assets/themes/tema_ctk.json")
      
 root = App()
